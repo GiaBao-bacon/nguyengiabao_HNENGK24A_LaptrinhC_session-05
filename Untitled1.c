c4e0f72a6fab1e08457ebb5fdbba5a6d467f19dd
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-05-09 10:48:27 +0700

    upload bài 1

---
+++ Untitled1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("In ra cac so tu 1 den 100 theo thu tu giam dan:\n");
+  int i; 
+  for (i = 100; i >= 1; i--) {
+    printf("%d ", i);
+  }
+  printf("\n");
+  return 0;
+}
